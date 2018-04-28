7b378983ec7f8465c3968fd576e8027e054eb806
dmitmel <dmytro.meleshko@gmail.com>
2018-04-28 12:28:37 +0300

    improve 'getMessageType' tests

--- __tests__/getMessageType.js
+++ __tests__/getMessageType.js
@@ -2,15 +2,25 @@ const getMessageType = require('../lib/getMessageType');
 
 const { MESSAGE_TYPES } = getMessageType;
 
-it('exports available message types an array of strings', () => {
-  expect(MESSAGE_TYPES).toEqual(expect.arrayContaining([expect.any(String)]));
-});
+describe('telegraf-update-logger/lib/getMessageType', () => {
+  describe('export const MESSAGE_TYPES', () => {
+    it('is an array of strings', () => {
+      expect(MESSAGE_TYPES).toEqual(
+        expect.arrayContaining([expect.any(String)])
+      );
+    });
+  });
 
-describe('getMessageType', () => {
-  MESSAGE_TYPES.forEach(type => {
-    it(`works for '${type}' messages`, () => {
-      const result = getMessageType({ [type]: {} });
-      expect(result).toBe(type);
+  describe('export default function getMessageType', () => {
+    MESSAGE_TYPES.forEach(type => {
+      it(`works for '${type}' messages`, () => {
+        // given:
+        const update = { [type]: { foo: 'bar' } };
+        // when:
+        const result = getMessageType(update);
+        // then:
+        expect(result).toBe(type);
+      });
     });
   });
 });
