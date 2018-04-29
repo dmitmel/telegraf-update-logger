36906c34b61660a2b91f790e39d23c5148ea3918
dmitmel <dmytro.meleshko@gmail.com>
2018-04-29 11:04:13 +0300

    add ignored files to .npmignore

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./lib/middleware');
+const middleware = require('./lib/middleware');
+const format = require('./lib/format');
+
+module.exports = Object.assign(middleware, { format });
