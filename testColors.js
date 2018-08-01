66850cd5a728655ec31c9929087ca0b330b57088
dmitmel <dmytro.meleshko@gmail.com>
2018-08-01 09:46:05 +0300

    update example image

---
+++ testColors.js
@@ -0,0 +1,11 @@
+const format = require('./lib/format');
+const testUpdates = require('./__tests__/testUpdates.json');
+
+console.log();
+
+Object.keys(testUpdates).forEach(name => {
+  const update = testUpdates[name];
+  console.log(format(update, { colors: true }));
+});
+
+console.log();
